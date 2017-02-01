3ba7a69024b717b76671e94d8dd2ef25c79b9189
monika <monika@monika-Inspiron-3520>
2017-02-01 16:31:04 -0500

    Commit Again

--- BIGDATA/src/Test.java
+++ BIGDATA/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Commit");
+		System.out.println("Playing with it!");
 	}
 
 }
